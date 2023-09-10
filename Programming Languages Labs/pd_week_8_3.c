81b332e6fb7032ef9f0079bbc55aff92b30965b7
ledger <76178128+esmnralican@users.noreply.github.com>
2023-09-10 22:12:29 +0300

    Create pd_week_8_3.c

---
+++ Programming Languages Labs/pd_week_8_3.c
@@ -0,0 +1,19 @@
+// Write C code to find the largest of three numbers A, B, and C
+
+#include <stdio.h>
+
+int main() {
+    int A, B, C;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &A, &B, &C);
+
+    if (A >= B && A >= C) {
+        printf("%d is the largest\n", A);
+    } else if (B >= A && B >= C) {
+        printf("%d is the largest\n", B);
+    } else {
+        printf("%d is the largest\n", C);
+    }
+
+    return 0;
+}
